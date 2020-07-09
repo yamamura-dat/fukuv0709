c7aafa01a4dfc9c14b1c8b955845c5ddde2b1de5
satthubongdem <49702341+satthubongdem@users.noreply.github.com>
2020-07-09 09:40:04 +0900

    完成

--- fukuv0709/Form1.Designer.cs
+++ fukuv0709/Form1.Designer.cs
@@ -39,7 +39,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(204, 99);
+            this.label1.Location = new System.Drawing.Point(205, 117);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 19);
             this.label1.TabIndex = 0;
